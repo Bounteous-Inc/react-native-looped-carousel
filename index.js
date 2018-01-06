8f084b78c6445b34b20addc40ceba1fc49e0ee74
jburich <jason.burich@hs2solutions.com>
2018-01-06 09:08:36 -0600

    Using Native Layouts instead of container measurements

--- index.js
+++ index.js
@@ -186,12 +186,11 @@ export default class Carousel extends Component {
     this._setUpTimer();
   }
 
-  _onLayout = () => {
-    this.container.measure((x, y, w, h) => {
-      this.setState({ size: { width: w, height: h } });
-      // remove setTimeout wrapper when https://github.com/facebook/react-native/issues/6849 is resolved.
-      setTimeout(() => this._placeCritical(this.state.currentPage), 0);
-    });
+  _onLayout = (event) => {
+    const {height, width} = event.nativeEvent.layout;
+    this.setState({size: {width, height}});
+    // remove setTimeout wrapper when https://github.com/facebook/react-native/issues/6849 is resolved.
+    setTimeout(() => this._placeCritical(this.state.currentPage), 0);
   }
 
   _clearTimer = () => {
